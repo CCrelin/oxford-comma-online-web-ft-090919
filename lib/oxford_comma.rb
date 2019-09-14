b28be242c12792d63aafacf78eb8dbc426c721b2
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-14 03:22:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   new = array.join(" and ")
   if array.length >= 3
-    array[-1] = "and #{array.last}"
+    array[-1] = "and #{array[-1]}"
     new = array.join(", ")
   end
   
